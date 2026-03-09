602e5a4a2c284d20797a87ba0abcda47a5e02af7
Priyajit60 <priyajitsarkar90@gmail.com>
2026-03-09 17:47:31 +0530

    feat: implement branch management API and UI for creating, editing, and toggling active status

---
+++ src/api/branch.js
@@ -0,0 +1,70 @@
+import { authorizedFetch } from './auth';
+
+// ==============================|| BRANCH MASTER API ||============================== //
+
+// Expected response: array of {
+//   id: number, // e.g. 9007199254740991 (Number.MAX_SAFE_INTEGER)
+//   name: string,
+//   code: string,
+//   address: string,
+//   pincode: string,
+//   keyContactPersonName: string,
+//   keyContactPersonPhone: string,
+//   active: boolean
+// }
+
+export async function getBranches() {
+  // eslint-disable-next-line no-console
+  console.log('Calling GET /api/v1/master/branch/list');
+  return authorizedFetch('/api/v1/master/branch/list', {
+    method: 'GET'
+  });
+}
+
+export async function createBranch({ name, code, address, pincode, keyContactPersonName, keyContactPersonPhone }) {
+  // eslint-disable-next-line no-console
+  console.log('Calling POST /api/v1/master/branch/create', {
+    name,
+    code,
+    address,
+    pincode,
+    keyContactPersonName,
+    keyContactPersonPhone
+  });
+  return authorizedFetch('/api/v1/master/branch/create', {
+    method: 'POST',
+    body: JSON.stringify({ name, code, address, pincode, keyContactPersonName, keyContactPersonPhone })
+  });
+}
+
+export async function getBranchById(id) {
+  // eslint-disable-next-line no-console
+  console.log(`Calling GET /api/v1/master/branch/edit/${id}`);
+  return authorizedFetch(`/api/v1/master/branch/edit/${id}`, {
+    method: 'GET'
+  });
+}
+
+export async function editBranch(id, { name, code, address, pincode, keyContactPersonName, keyContactPersonPhone }) {
+  // eslint-disable-next-line no-console
+  console.log(`Calling PUT /api/v1/master/branch/edit/${id}`, {
+    name,
+    code,
+    address,
+    pincode,
+    keyContactPersonName,
+    keyContactPersonPhone
+  });
+  return authorizedFetch(`/api/v1/master/branch/edit/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ name, code, address, pincode, keyContactPersonName, keyContactPersonPhone })
+  });
+}
+
+export async function toggleBranchActive(id, active) {
+  // eslint-disable-next-line no-console
+  console.log(`Calling PATCH /api/v1/master/branch/disable/${id}/${active}`);
+  return authorizedFetch(`/api/v1/master/branch/disable/${id}/${active}`, {
+    method: 'PATCH'
+  });
+}
